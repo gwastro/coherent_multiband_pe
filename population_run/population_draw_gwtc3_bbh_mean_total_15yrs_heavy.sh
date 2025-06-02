70402aed92771d948835523814076730c19dc985
Shichao Wu <wushichao@mail.bnu.edu.cn>
2025-06-02 23:19:26 +0200

    add population run files

---
+++ population_run/population_draw_gwtc3_bbh_mean_total_15yrs_heavy.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+gen_multiverse() {
+    local seed=$1
+    echo "Running gen_multiverse with seed $seed"
+    
+#    if ! pycbc_create_injections --verbose \
+#        --config-files ./gwtc3_bbh_mean_total_15yrs.ini \
+#        --ninjections 989590 \
+#        --seed "$seed" \
+#        --output-file "sobhb_gwtc3_bbh_mean_total_15yrs_seed_${seed}_989590.hdf" \
+#        --variable-params-section variable_params \
+#        --static-params-section static_params \
+#        --dist-section prior \
+#        --force; then
+#        echo "Error creating injections for seed $seed" >&2
+#        return 1
+#    fi
+
+    if ! ./check_optimal_snr_duration_heavy \
+        --seed "$seed" \
+        --dir-path /work/shichao.wu/Area51/coherent_multiband_prod \
+        --injection-file-name "sobhb_gwtc3_bbh_mean_total_15yrs_seed_${seed}_989590" \
+        --t-lisa-obs-cases "3 7.5" \
+        --t-3g-wait-cases "5 5"; then
+        echo "Error checking optimal SNR duration for seed $seed" >&2
+        return 1
+    fi
+
+    echo "Completed gen_multiverse with seed $seed"
+}
+
+export -f gen_multiverse
+
+seq 21 120 | xargs -P 20 -I {} bash -c 'gen_multiverse "$@"' _ {}
---
+++ population_run/submit_lisa_seed_1_obs_1yr_wait_5yrs.sh
@@ -0,0 +1,9 @@
+./pycbc_make_lisa_inference \
+--population-draw-file sobhb_gwtc3_bbh_mean_total_15yrs_seed_1_989590.hdf \
+--injection-file-folder /work/shichao.wu/Area51/coherent_multiband_prod/population_run/multiverse_seed_1/injection_above_snr_threshold_seed_1_obs_1.0yrs_wait_5.0yrs/ \
+--injection-snr-duration-file snr_seed_1_obs_1.0yrs_wait_5.0yrs.csv \
+--lisa-obs-duration 1 \
+--inference-config prior_lisa.ini \
+--config-files workflow_lisa.ini \
+--workflow-name lisa_workflow_seed_1_obs_1yr_wait_5yrs_nessai \
+--output-dir ./output_lisa_seed_1_obs_1yr_wait_5yrs_nessai --submit-now
